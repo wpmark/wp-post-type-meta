3549ecd7639bd2547a8b0ab36415b5f51988b449
Mark Wilkinson <mark@markwilkinson.me>
2015-06-23 09:42:29 +0100

    Improve wpptm_get_field
    Prevent undefined index errors with wp debug turned on and the field
    does not exist.

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://markwilkinson.me/saythanks
 Tags: custom post type, meta, options
 Requires at least: 4.0
 Tested up to: 4.1
-Stable tag: 0.9.3
+Stable tag: 0.9.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -86,6 +86,9 @@ In the above example the custom post type was called `my_cpt` so you would acces
 1. The post type meta screen shows all the fields added to this post type.
 
 == Changelog ==
+
+= 0.9.4 =
+* Fix an undefined index error when WP_DEBUG is true and the field being called does not exist 
 
 = 0.9.3 =
 * Strip slashes when saving wysiwyg input.
--- wp-post-type-meta.php
+++ wp-post-type-meta.php
@@ -2,11 +2,22 @@
 /*
 Plugin Name: WP Post Type Meta
 Description: Adds an admin sub menu to allow for a post type description to be added, ideal for showing at the top of the custom post type archive page. Through extensibility features the plugin also allows other post type meta information to be stored on a per post type basis.
-Version:     0.9.3
+Version:     0.9.4
 Author:      Mark Wilkinson
 Author URI:  http://markwilkinson.me
 Text Domain: wpptm
 License:     GPL v2 or later
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
 */
 
 /*
@@ -71,26 +82,36 @@ function wpptm_get_field( $field, $post_type = '', $default = '' ) {
 	/* get the fields for this post type from options */
 	$options = get_option('wpptm_' . $post_type );
 	
-	/* get the desired field from the returned post type data */
-	$setting = $options[ $field ];
+	/* check if the field exists in the array */
+	if( array_key_exists( $field, $options ) ) {
+		
+		/* get the desired field from the returned post type data */
+		$setting = $options[ $field ];
+		
+		/* check we have a field to return */
+		if( empty( $setting ) ) {
+			
+			/* do we have a default to return */
+			if( $default != '' ) {
+				
+				/* set the default to be returned */
+				$setting = $default;
+			
+			/* no setting or default */
+			} else {
+				
+				/* set to false */
+				$setting = false;
+				
+			}
 	
-	/* check we have a field to return */
-	if( empty( $setting ) ) {
-		
-		/* do we have a default to return */
-		if( $default != '' ) {
-			
-			/* set the default to be returned */
-			$setting = $default;
-		
-		/* no setting or default */
-		} else {
-			
-			/* set to false */
-			$setting = false;
-			
 		}
-
+		
+	} else {
+		
+		/* set the default to be returned */
+		$setting = $default;
+		
 	}
 	
 	/* return our field */
